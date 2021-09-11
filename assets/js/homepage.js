bfc122b06db863d0778b3a00ed49400a4df24ca2
Ben Kaan <benkaan001@gmail.com>
2021-09-10 21:06:52 -0500

    added languages buttons and links

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -90,5 +90,21 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  fetch(apiUrl).then(function(response){
+    if (response.ok) {
+      response.json().then(function(data){
+        displayRepos(data.itmes, language);
+
+      });
+      
+    }else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+  
+};
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
